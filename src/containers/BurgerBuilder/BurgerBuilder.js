dda01c4f590bc5f8501476859470f3ff75b463a3
abdnafees <abdullahnafees23@gmail.com>
2018-08-08 19:20:58 +0500

    Burger Builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildConrols/BuildControls';
+class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+    }
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </Aux>
+        );
+    };
+};
+
+export default BurgerBuilder;
